b1397dc495ec32660df7f96f711fd9f4e4d026f5
G T Sai Krishna Reddy <saikrishnaa.97@gmail.com>
2021-11-20 13:05:17 +0530

    Added firebase client changes

--- src/ShareMarketTraining/FirebaseClient.py
+++ src/ShareMarketTraining/FirebaseClient.py
@@ -2,6 +2,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from . import KubernetesClient
+from ShareMarketTraining.rest_client.rest_client import Rest_client
+import uuid
 
 class FirebaseClient():
     def __init__(self):
@@ -18,5 +20,29 @@ class FirebaseClient():
     def getTradeByUserId(self, uid):
         return self.trade_ref.get().get(uid)
 
-    def updateUser(self):
-        return 1
+    def updateTrade(self,tradeData):
+        try:
+            existing = self.trade_ref.child(tradeData['uid'])
+            existing.update(tradeData)
+            response = {"status":"success"}
+        except Exception as e:
+            response = {"status":"And Exception occured "+str(e)}
+        return response
+
+    def buyShares(self, tradeData,userId):
+        try:
+            uid = str(uuid.uuid4())
+            tradeData['uid'] = uid
+            tradeData['soldAt'] = -1
+            tradeData["status"] = "HOLDING"
+            stockStatus = Rest_client().get_stock_status(tradeData["stockSymbol"])
+            if float(stockStatus['NSE']['price_info']['lastPrice']) > float(stockStatus['BSE']['price_info']['LTP']):
+                tradeData['purchasedAt'] = float(stockStatus['BSE']['price_info']['LTP'])
+            else:
+                tradeData['purchasedAt'] = float(stockStatus['NSE']['price_info']['lastPrice'])
+            self.trade_ref.child(userId).push({uid:tradeData})
+        except Exception as e:
+            response = {"status": "And Exception occured " + str(e)}
+        return response
+
+
