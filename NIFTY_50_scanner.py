96bd4448c9122bda815ed19c05ab1018a1637d10
G T Sai Krishna Reddy <saikrishnaa.97@gmail.com>
2023-02-02 12:53:12 +0530

    Update NIFTY_50_scanner.py

--- NIFTY_50_scanner.py
+++ NIFTY_50_scanner.py
@@ -8,7 +8,7 @@ def get_scanner_data(args):
 #  print('Content-Type:text/html\r\n\r\n')
   print('Content-type: application/json\r\n\r\n') # the mime-type header.
   url = "https://www.nseindia.com/"
-  headers = {"user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (K                                                                                        HTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36", "accept-encoding": "gzip,                                                                                         deflate, br","accept-language": "en-GB,en-US;q=0.9,en;q=0.8,la;q=0.7","sec-ch-ua                                                                                        -platform": "Linux", "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";                                                                                        Not A Brand";v="99"'}
+  headers = {"user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36", "accept-encoding": "gzip,deflate, br","accept-language": "en-GB,en-US;q=0.9,en;q=0.8,la;q=0.7","sec-ch-ua-platform": "Linux", "sec-ch-ua": '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"'}
   conn = requests.Session()
   try:
    response = conn.get(url,headers=headers)
@@ -21,8 +21,8 @@ def get_scanner_data(args):
       l = args['list'][0]
   else:
       l = "NIFTY 100"
-  response = conn.get(url+"api/equity-stockIndices?index="+l,headers=headers,coo                                                                                        kies=cookies)
-  #response = session.get(url+"api/equity-stockIndices?index=NIFTY TOTAL MARKET"                                                                                        ,headers=headers,cookies=cookies)
+  response = conn.get(url+"api/equity-stockIndices?index="+l,headers=headers,cookies=cookies)
+  #response = session.get(url+"api/equity-stockIndices?index=NIFTY TOTAL MARKET",headers=headers,cookies=cookies)
   data = response.json()
   result = {}
   result['data'] = []
