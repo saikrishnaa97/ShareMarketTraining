8ba28005a23438ab2d8c2ee20d05889e1852f6cc
G T Sai Krishna Reddy <saikrishnaa.97@gmail.com>
2023-02-27 16:59:10 +0530

    Update firebase_client.py

--- firebase_client.py
+++ firebase_client.py
@@ -3,6 +3,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from datetime import datetime
 
 
 cred = credentials.Certificate("/usr/share/httpd/sample/fbSA.json")
@@ -16,3 +17,18 @@ firebase_admin.initialize_app(cred, {
 def get_portfolio(userId):
     ref = db.reference('Trades/'+userId)
     return ref.get()
+
+def get_userDetails(userId):
+    ref = db.reference('Users/'+userId)
+    return ref.get()
+
+def update_portfolio(userId,data):
+    user_ref = db.reference('Users/'+userId)
+    portfolio_ref = db.reference('Trades/'+userId)
+    user_data = user_ref.get()
+    portfolio_data = portfolio_ref.get()
+    user_data['lastUpdatedTimestamp'] = datetime.now().strftime("%s")
+    for k,v in portfolio_data.items():
+        portfolio_data[k] = data['portfolio'][k]
+    portfolio_ref.update(portfolio_data)
+    user_ref.update({"lastUpdatedTimestamp":int(datetime.now().strftime("%s"))})
