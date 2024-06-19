3e80572113bc8d8875922c3c89a738d73d8605ac
G T Sai Krishna Reddy <saikrishnaa.97@gmail.com>
2024-06-19 11:24:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 yum install -y httpd
 systemctl enable httpd
+systemctl restart httpd
+firewall-cmd --add-service=http --permanent
+firewall-cmd --add-service=https --permanent
+firewall-cmd --reload
