e439e79a59d182884df315138367944399d504ad
G T Sai Krishna Reddy <saikrishnaa.97@gmail.com>
2024-06-19 11:58:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ systemctl restart httpd
 firewall-cmd --add-service=http --permanent
 firewall-cmd --add-service=https --permanent
 firewall-cmd --reload
+
+
+yum -y install gcc-c++ python3-devel python3-pip python3 python3-setuptools
